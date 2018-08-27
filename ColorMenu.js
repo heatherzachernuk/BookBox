8280c9666865bff1828326e215838dbe58a1a635
Heather Zachernuk <35938539+heatherzachernuk@users.noreply.github.com>
2018-08-26 20:08:39 -0700

    .

--- ColorMenu.js
+++ ColorMenu.js
@@ -10,6 +10,9 @@ class ColorMenu {
 		this.coverButton = document.getElementById("cover-color-button");
 		this.colorSourceBox = document.getElementById("color-source-box");
 		this.palettePicker = document.getElementById("palette-picker");
+		this.brightness = document.getElementById("brightness");
+		this.hue = document.getElementById("hue");
+		this.currentColor = config.backgroundColor;
 		this.imagePicker = document.getElementById("image-picker");
 
 		this.stripesOn = document.getElementById("stripes-on");
@@ -72,7 +75,9 @@ class ColorMenu {
 		this.palettePicker.style.top = this.setColorBox.getBoundingClientRect().top + "px";
 		this.palettePicker.style.left = this.setColorBox.getBoundingClientRect().left + "px";
 		this.drawPalette();
-		this.palettePicker.addEventListener("mousemove", event=>this.pickPaletteColor(event));
+		hue.addEventListener("mousemove", event=> this.pickColor(event, hue));
+		brightness.addEventListener("mousemove", event=> this.pickColor(event, brightness));
+		// this.palettePicker.addEventListener("mousemove", event=>this.pickPaletteColor(event));
 	}
 
 	showImage(){
@@ -96,42 +101,52 @@ class ColorMenu {
 	}
 
 	drawPalette(){
-	  var ctx = this.palettePicker.getContext("2d");           
-	  var rainbowGradient = ctx.createLinearGradient( 0, 200, 200, 200);
-	  rainbowGradient.addColorStop(0, '#ff0000');
-	  rainbowGradient.addColorStop(1/8, '#ff8000');
-	  rainbowGradient.addColorStop(2/8, '#ffff00');
-	  rainbowGradient.addColorStop(3/8, '#00ff00');
-	  rainbowGradient.addColorStop(4/8, ' #0066ff');
-	  rainbowGradient.addColorStop(5/8, '#6600ff');
-	  rainbowGradient.addColorStop(6/8, '#ff00ff');
-	  rainbowGradient.addColorStop(7/8, '#ff0000');
-	  rainbowGradient.addColorStop(1, '#000000');
-	  ctx.fillStyle = rainbowGradient;
-	  ctx.fillRect(0, 0, 200, 200);
-	  
-	  var whiteGradient = ctx.createLinearGradient(200, 200, 200, 0);
-	  whiteGradient.addColorStop(0, 'hsla(0, 0%, 100%, 0)');
-	  whiteGradient.addColorStop(1, 'hsla(0, 0%, 100%, 0.95)');
-	  ctx.fillStyle = whiteGradient;
-	  ctx.fillRect(0, 0, 200, 200);
-	  
-	  var blackGradient = ctx.createLinearGradient(200, 200, 200, 0);
-	  blackGradient.addColorStop(0, 'hsla(0, 0%, 0%, 1)');
-	  blackGradient.addColorStop(1, 'hsla(0, 0%, 0%, 0)');
-	  ctx.fillStyle = blackGradient;
-	  ctx.fillRect(0, 0, 200, 200);
+		this.drawBright();
+		this.drawHue();
 	}
 
-	pickPaletteColor(color){
-		this.palettePicker.style.cursor = "crosshair";
-		var paletteCtx = this.palettePicker.getContext("2d");
-		var offsetX = this.palettePicker.getBoundingClientRect().left;
-	  	var offsetY = this.palettePicker.getBoundingClientRect().top;
-		var colorValue = paletteCtx.getImageData(color.clientX - offsetX, color.clientY - offsetY, 1, 1).data;
-		var rgba = 'rgba(' + colorValue[0] + ', ' + colorValue[1] + ', ' + colorValue[2] + ', ' + (colorValue[3] / 255) + ')';
-		// console.log(rgba);
-		this.updateColors(rgba);
+	drawBright(){
+		var brightCtx = this.brightness.getContext("2d");
+		var brightGradient = brightCtx.createLinearGradient(0, 0, 0, 200);
+		brightGradient.addColorStop(0, "rgba(0,0,0,0)");
+		brightGradient.addColorStop(1, "black");
+		var colorGradient = brightCtx.createLinearGradient(0, 0, 200, 0);
+		colorGradient.addColorStop(0, "white");
+		colorGradient.addColorStop(1, this.currentColor);
+		brightCtx.fillStyle = colorGradient;
+		brightCtx.fillRect(0, 0, 200, 200);  
+		brightCtx.fillStyle = brightGradient;
+		brightCtx.fillRect(0, 0, 200, 200); 
+	}
+
+	drawHue(){
+		var hueCtx = this.hue.getContext("2d");
+		var hueGradient = hueCtx.createLinearGradient(0, 0, 0, 200);
+		hueGradient.addColorStop(0/6, '#ff0000');
+		hueGradient.addColorStop(1/6, '#ff8000'); 
+		hueGradient.addColorStop(2/6, '#ffff00'); 
+		hueGradient.addColorStop(3/6, '#00cc00'); 
+		hueGradient.addColorStop(4/6, ' #0066ff'); 
+		hueGradient.addColorStop(5/6, '#6600ff'); 
+		hueGradient.addColorStop(6/6, '#ff00ff'); 
+		hueCtx.fillStyle = hueGradient;
+		hueCtx.fillRect(0, 0, 50, 200);
+	}
+
+	pickColor(color, element){
+		element.style.cursor = "crosshair";
+		var ctx = element.getContext("2d");
+		var offsetX = element.getBoundingClientRect().left;
+		var offsetY = element.getBoundingClientRect().top;
+		var colorValue = ctx.getImageData(color.clientX - offsetX, color.clientY - offsetY, 1, 1).data;
+		var rgba = 'rgba(' + colorValue[0] + ', ' + colorValue[1] + ', ' + colorValue[2] + ', ' + (colorValue[3] / 255) + ')'; 
+		if(element === hue){
+			this.currentColor = rgba;
+			this.drawBright();
+		}
+		if(element === brightness){
+			this.updateColors(rgba);
+		}
 	}
 
 	updateColors(rgba) {
@@ -151,6 +166,4 @@ class ColorMenu {
 		var rgba = 'rgba(' +colorValue[0]+', '+colorValue[1]+', '+colorValue[2]+', '+(colorValue[3]/255)+')';
 		this.updateColors(rgba);
 	}
-
-
 }
---
+++ colorReconfig.js
@@ -0,0 +1,73 @@
+var brightness = document.getElementById("brightness");
+var hue = document.getElementById("hue");
+var currentColor = "rgb(212, 255, 191)"
+
+hue.addEventListener("click", event=> hueListen(event));
+brightness.addEventListener("click", event=> brightListen(event));
+
+function hueListen(){
+  hue.addEventListener("mousemove", event=> pickHue(event));
+}
+
+function brightListen(){
+  brightness.addEventListener("mousemove", event=> pickColor(event));
+}
+
+drawBright();
+hueBow();
+
+var g = brightness.getContext('2d')
+
+function drawBright(){
+  var brightCtx = brightness.getContext("2d");
+ 
+  var brightGradient = brightCtx.createLinearGradient(0, 0, 0, 200);
+  brightGradient.addColorStop(0, "rgba(0,0,0,0)");
+  brightGradient.addColorStop(1, "black");
+  
+  var colorGradient = brightCtx.createLinearGradient(0, 0, 200, 0);
+  colorGradient.addColorStop(0, "white");
+  colorGradient.addColorStop(1, currentColor);
+  brightCtx.fillStyle = colorGradient;
+  brightCtx.fillRect(0, 0, 200, 200);  
+  brightCtx.fillStyle = brightGradient;
+  brightCtx.fillRect(0, 0, 200, 200);  
+}
+
+function hueBow(){
+  var hueCtx = hue.getContext("2d");
+  var hueGradient = hueCtx.createLinearGradient(0, 0, 0, 200);
+  hueGradient.addColorStop(0/6, '#ff0000');
+  hueGradient.addColorStop(1/6, '#ff8000'); 
+  hueGradient.addColorStop(2/6, '#ffff00'); 
+  hueGradient.addColorStop(3/6, '#00cc00'); 
+  hueGradient.addColorStop(4/6, ' #0066ff'); 
+  hueGradient.addColorStop(5/6, '#6600ff'); 
+  hueGradient.addColorStop(6/6, '#ff00ff'); 
+  hueCtx.fillStyle = hueGradient;
+  hueCtx.fillRect(0, 0, 50, 200);
+}
+
+function pickHue(color){
+  hue.style.cursor = "crosshair";
+  var huePickCtx = hue.getContext("2d");
+  var offsetX = hue.getBoundingClientRect().left;
+  var offsetY = hue.getBoundingClientRect().top;
+  var colorValue = huePickCtx.getImageData(color.clientX - offsetX, color.clientY - offsetY, 1, 1).data;
+  var rgba = 'rgba(' + colorValue[0] + ', ' + colorValue[1] + ', ' + colorValue[2] + ', ' + (colorValue[3] / 255) + ')';
+  console.log(rgba);
+  currentColor = rgba;
+  drawBright();
+}
+
+function pickColor(color){
+  brightness.style.cursor = "crosshair";
+  var brightCtx = brightness.getContext("2d");
+  var offsetX = brightness.getBoundingClientRect().left;
+  var offsetY = brightness.getBoundingClientRect().top;
+  var colorValue = brightCtx.getImageData(color.clientX - offsetX, color.clientY - offsetY, 1, 1).data;
+  var rgba = 'rgba(' + colorValue[0] + ', ' + colorValue[1] + ', ' + colorValue[2] + ', ' + (colorValue[3] / 255) + ')';
+  console.log("set config", rgba);
+
+}
+
